fac13ba7a07528d3b68b09ea26ac22a922e1a809
justsimplycoder <justsimplycoder@gmail.com>
2023-05-27 18:41:25 +0400

    Добавление: запрос на сервер с помощью fetch api

--- tests/api.test.js
+++ tests/api.test.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const { faker } = require('@faker-js/faker');
 
 describe('Тестируем пользователей', () => {
-	it('Нельзя создать пользователя с не правильноым паролем', async () => {
+	it.only('Нельзя создать пользователя с не правильноым паролем (axios)', async () => {
 		try {
 			await axios.post('https://bookstore.demoqa.com/Account/v1/User', {
 				"userName": "string",
@@ -19,8 +19,27 @@ describe('Тестируем пользователей', () => {
 		}
 	});
 
-	// it.only - пропускает тест
-	// it.skip - не запускает тест, в ожидании
+	it.only('Нельзя создать пользователя с не правильноым паролем (fetch api)', async () => {
+		let user = {
+			userName: "string",
+			password: "string"
+		};
+		let response = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			},
+			body: JSON.stringify(user)
+		});
+		const errorData = await response.json()
+		expect(response.status).to.be.equal(400);
+		expect(response.status).not.to.be.equal(200);
+		expect(errorData).to.be.deep.equal({
+			"code": "1300",
+			"message": "Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer."
+		});
+	});
+
 	it('Нельзя создать пользователя, если имя уже занято', async () => {
 		try {
 			await axios.post('https://bookstore.demoqa.com/Account/v1/User', {
@@ -43,7 +62,7 @@ describe('Тестируем пользователей', () => {
 		expect(response.data.username).to.be.equal(userName);
 	});
 
-	it.only('Получение токена', async () => {
+	it('Получение токена', async () => {
 		const response = await axios.post('https://bookstore.demoqa.com/Account/v1/GenerateToken', {
 			userName: "Jon",
 			password: "P@ssw0rd"
